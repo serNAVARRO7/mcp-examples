6e9161bd4e0613da092a2a6c06abe2e2c905aedd
serNAVARRO7 <sernavarrops3@gmail.com>
2025-07-11 13:33:12 +0200

    feat: Add weather server example

---
+++ 01-start/main.ts
@@ -0,0 +1,49 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+
+const server = new McpServer({
+  name: "Demo",
+  version: "1.0.0",
+});
+
+server.tool(
+  "fetch-weather",
+  "Tool to fetch the weather",
+  {
+    city: z.string().describe("City name"),
+  },
+  async ({ city }) => {
+    const response = await fetch(
+      `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10&language=en&format=json`
+    );
+    const data = await response.json();
+
+    if (!data?.results) {
+      return {
+        content: [
+          {
+            type: "text",
+            text: `No information`,
+          },
+        ],
+      };
+    }
+
+    const { latitude, longitude } = data.results[0];
+    const weatherResponse =  await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&current=temperature_2m,precipitation,rain,is_day&forecast_days=1`);
+    const weatherData = await weatherResponse.json();
+
+    return {
+      content: [
+        {
+          type: "text",
+          text: JSON.stringify(weatherData, null, 2),
+        },
+      ],
+    };
+  }
+);
+
+const transport = new StdioServerTransport();
+await server.connect(transport);
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # mcp-examples
+
+### MCP Inspector is an interactive developer tool for testing and debugging MCP servers:
+```bash
+cd ./01-start
+npx -y @modelcontextprotocol/inspector npx -y tsx main.ts
+```
+
+## Server Examples:
+https://github.com/modelcontextprotocol/servers
+
+### Find awesome MCP Servers:
+https://mcp.so/
